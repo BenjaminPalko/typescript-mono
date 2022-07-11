7fbb9bc1ad518e7964329abaa008f61b97299612
Benjamin <benjaminpalko@hotmail.com>
2022-07-11 12:42:38 -0400

    added mongodb container

--- datastore/src/main.ts
+++ datastore/src/main.ts
@@ -1,6 +1,11 @@
-import {connect} from 'amqplib';
+import amqp from 'amqplib';
+import mongoose from 'mongoose';
 
 const amqp_uri = process.env.RABBITMQ_URI ?? 'amqp://localhost:5672';
-connect(amqp_uri)
+amqp.connect(amqp_uri)
     .then(value => console.log(`Connected to AMQP server ${amqp_uri}`))
     .catch(error => console.error(error));
+const database_uri = process.env.DATABASE_URI ?? 'mongodb://localhost:27017';
+mongoose.connect(database_uri)
+	.then(value => console.log(`Connected to Mongodb server ${database_uri}`))
+	.catch(reason => console.error(reason));
